bb3c0e4102ddaff77b7b69832ae5a2a07992da29
Omkar thorat <114384305+omthorat@users.noreply.github.com>
2024-06-20 11:37:39 +0000

    feat: added chat api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,12 @@ const {
   authUser,
   allUsers,
 } = require("../controllers/userControllers");
-// const { protect } = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-// router.route("/").get(protect, allUsers);
-router.post("/", registerUser);
+router.route("/").get(protect, allUsers);
+router.post("/", registerUser)
 router.post("/login", authUser);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const chats = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const userChats= require("./routes/userChats");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
 const app = express();
@@ -12,7 +13,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("app started");
 });
+
+
 app.use("/api/user", userRoutes);
+app.use("/api/chats",userChats)
+
 
 app.use(notFound);
 app.use(errorHandler);
